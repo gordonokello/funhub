9972a1bb0477f72bc425520315b8ef5b05e86975
gordon <gordon.okello@imbank.co.ke>
2025-10-13 06:27:16 +0300

    Add scheduler for when email go out

---
+++ src/main/kotlin/co/ke/inmbank/funhub/scheduler/FunHubScheduler.kt
@@ -0,0 +1,18 @@
+package co.ke.inmbank.funhub.scheduler
+
+import co.ke.inmbank.funhub.service.SecretSantaService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class FunHubScheduler(
+    private val secretSantaService: SecretSantaService,
+) {
+    private val logger = LoggerFactory.getLogger(FunHubScheduler::class.java)
+
+    @Scheduled(cron = "\${inmbank.funhub.cron}", zone = "Africa/Nairobi")
+    fun sendSecretSantaEmail() {
+        secretSantaService.sendSantaEmail()
+    }
+}
